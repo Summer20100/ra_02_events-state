10e881180ebf4c460487b9820ee665c276f56c98
Dmitrii Bulatov <bulati484@gmail.com>
2023-08-05 09:12:00 +0300

    correct PlojectListModule

--- src/components/Filter/Portfolio/ProjectList/ProjectList.jsx
+++ src/components/Filter/Portfolio/ProjectList/ProjectList.jsx
@@ -3,8 +3,8 @@ import s from './ProjectList.module.css'
 const ProjectList = ({ projects, filter }) => {
 
   let projectItem = [...projects].map((itm, ind) =>
-    <div className={s.img} category={itm.category} key={ind}>
-      <img src={itm.img} alt='Фото' />
+    <div className={s.imgBox} category={itm.category} key={ind}>
+      <img className={s.img} src={itm.img} alt='Фото' />
     </div>
   )
 
